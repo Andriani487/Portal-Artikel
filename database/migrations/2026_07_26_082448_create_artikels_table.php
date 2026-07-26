68f692685935b5a736a2e39c120154fe5e0a9175
Agungs007 <agsuradixx@gmail.com>
2026-07-26 22:23:04 +0700

    Fix migration for 'artikel' table structure
    Updated the 'artikels' table migration to use correct column names and types.

--- database/migrations/2026_07_26_082448_create_artikels_table.php
+++ database/migrations/2026_07_26_082448_create_artikels_table.php
@@ -5,23 +5,21 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+    {
     public function up(): void
     {
         Schema::create('artikel', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_artikel');
+            $table->string('judul');
+            $table-text('isi');
+            $table-string('penulis');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
-        Schema::dropIfExists('artikels');
+        Schema::dropIfExists('artikel');
     }
 };

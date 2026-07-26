8264541edfadd0c1306af8249751be26f574a67e
Agungs007 <agsuradixx@gmail.com>
2026-07-26 21:42:32 +0700

    Update Artikel model with table and primary key settings

--- app/Models/Artikel.php
+++ app/Models/Artikel.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
 class Artikel extends Model
 {
-    //
+    protected $table ='artikel';
+    protected $primaryKey = 'id_artikel';
+    protected $fillable = [
+    'judul',
+    'isi',
+    ;penulis',
+    ];
 }

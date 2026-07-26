610f021a0e40f08b9c7a9209912e1c90b1fb76ef
Agungs007 <agsuradixx@gmail.com>
2026-07-26 22:13:48 +0700

    Fix syntax error in Artikel model fillable array

--- app/Models/Artikel.php
+++ app/Models/Artikel.php
@@ -1,5 +1,6 @@
 <?php
-
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 class Artikel extends Model
 {
     protected $table ='artikel';
@@ -7,6 +8,6 @@ class Artikel extends Model
     protected $fillable = [
     'judul',
     'isi',
-    ;penulis',
+    'penulis',
     ];
 }
